81a6e543f196310fe359cfecd25c97c36d0c5023
AimanAJ <100359631+AimanAJ@users.noreply.github.com>
2022-07-18 20:38:06 +0300

    main

--- Project/routes/web.php
+++ Project/routes/web.php
@@ -22,3 +22,6 @@ Route::get('/', function () {
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
